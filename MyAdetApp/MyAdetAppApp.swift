389d10903537bc323f5d938498fe1662c677682c
Dastish <zhalgasdasik@gmail.com>
2025-12-08 23:38:55 +0500

    Initial Commit

---
+++ MyAdetApp/MyAdetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAdetAppApp.swift
+//  MyAdetApp
+//
+//  Created by Дастан Жалгас on 08.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct MyAdetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
